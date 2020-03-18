a156c31dba95921cdccbe43f67bf036b78cf904c
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-18 22:25:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,6 +13,10 @@ def link
   puts catchphrase
 end
 
-def all_phrases(all_phrases)
-  puts all_phrases
+describe "all_phrases" do
+  it "takes in an argument and puts out the catch phrase" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
 end
