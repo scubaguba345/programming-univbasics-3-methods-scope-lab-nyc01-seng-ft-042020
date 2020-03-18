49377f087b0b1a6ff33c15bc1f4aa60621fcb63e
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-18 22:55:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,3 +12,11 @@ end
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+describe "all_phrases" do
+  it "takes in an argument and puts out the catch phrase" do
+  puts phrase  
+  puts status
+  puts "It's Dangerous To Go Alone! Take This."
+  end
+end
