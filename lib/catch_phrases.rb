536bbf51f649dde13e1a5e3913ac655237540879
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-18 22:37:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,3 +12,11 @@ end
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+describe "all_phrases" do
+  it "takes in an argument and puts out the catch phrase" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
+end
